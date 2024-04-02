8210069eaa0395b493f334167d3a258798d64d8d
christinaw20 <68133503+christinaw20@users.noreply.github.com>
2024-04-02 19:24:24 -0400

    this better be it

--- App.js
+++ App.js
@@ -6,16 +6,10 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 
 const app = express()
-// app.use(cors());
-app.use(
-    cors({
-    origin: "https://a5--glistening-cucurucho-fcaf76.netlify.app",
-    credentials: true,
-    })
-    );
+app.use(cors());
 app.use(express.json());
-CourseRoutes(app);
 ModuleRoutes(app);
+CourseRoutes(app);
 Hello(app)
 Lab5(app);
 app.listen(process.env.PORT || 4000);
