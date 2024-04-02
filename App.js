711661b878302d33fb45f237ba9e3497b2f7907a
christinaw20 <68133503+christinaw20@users.noreply.github.com>
2024-04-02 18:44:20 -0400

    minor fix

--- App.js
+++ App.js
@@ -6,7 +6,13 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 
 const app = express()
-app.use(cors());
+// app.use(cors());
+app.use(
+    cors({
+    origin: "https://a5--glistening-cucurucho-fcaf76.netlify.app",
+    credentials: true,
+    })
+    );
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
