45b47a7534147476acf01dc6ba9cea65b06e50d0
christinaw20 <68133503+christinaw20@users.noreply.github.com>
2024-04-02 22:03:04 -0400

    last commit

--- App.js
+++ App.js
@@ -6,7 +6,10 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 
 const app = express()
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: ["http://localhost:4000", "https://a5--glistening-cucurucho-fcaf76.netlify.app"],
+}));
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
