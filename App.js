a0a041ad79579ab254879d04ee0060b21ae299a3
christinaw20 <68133503+christinaw20@users.noreply.github.com>
2024-04-15 19:18:56 -0400

    first commit

--- App.js
+++ App.js
@@ -4,13 +4,33 @@ import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
+import session from "express-session";
+import "dotenv/config";
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express()
 app.use(cors({
     credentials: true,
-    origin: ["http://localhost:4000", "https://a5--glistening-cucurucho-fcaf76.netlify.app"],
-}));
+    origin: process.env.FRONTEND_URL
+  }));
+  const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+  }
+  app.use(session(sessionOptions));
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Hello(app)
